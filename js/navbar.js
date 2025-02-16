7435a2c4cb11839cc6e2233905a7d2a293e2345f
sophiestroeve <sophiestroeve@proton.me>
2025-02-16 14:53:27 +0100

    big rework

---
+++ js/navbar.js
@@ -0,0 +1,8 @@
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar__menu');
+
+// Toggle menu functionality
+menu.addEventListener('click', function () {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+});
